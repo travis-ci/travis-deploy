4614d945195899e9537068ae805422a796e0e3ae
Edmund von der Burg <evdb@mysociety.org>
2012-09-28 15:07:52 +0100

    Don't skip verification of the SSL certificate

--- lib/travis/cli/secure_key.rb
+++ lib/travis/cli/secure_key.rb
@@ -27,7 +27,6 @@ module Travis
 
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         
         request = Net::HTTP::Get.new(uri.request_uri)
 
